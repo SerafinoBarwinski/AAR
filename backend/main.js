46070eb2e944bbdf7e574c85522d7620bbb9cda3
Serafino Barwinski <110348589+BraucheeinPsychologe@users.noreply.github.com>
2025-12-05 09:10:09 +0000

    First NodeJS Update

--- backend/main.js
+++ backend/main.js
@@ -1,17 +1,35 @@
-// Liste aller Adapter (Dateinamen oder IDs)
-const AdapterNames = ["ollama_adapter"];
-const AdapterLoaded = {};
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-async function loadAdapters(AdapterNames) {
-  if (AdapterLoaded = "{}") { console.error("Failed to load adapter: Already loaded"); return; }
-// Module dynamisch laden
-moduleNames.forEach(name => {
-  try {
-    modules[name] = require(`./modules/${name}.js`);
-    console.log(`Loaded adapter: ${name}`);
-  } catch (err) {
-    console.error(`Failed to load adapter '${name}':`, err.message);
-  }
-});
-  console.log(modules["ollama_adapter"].name); // "Ollama Adapter"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const modulesPath = path.join(__dirname, "..", "modules");
+
+let logging = process.argv.slice(2) || "default";
+
+const modules = await listModules()
+const modules_loaded = [];
+
+async function listModules() {
+  const files = await fs.readdir(modulesPath);
+  const jsFiles = files
+    .filter(file => file.endsWith(".js"))
+    .map(file => file.slice(0, -3));
+  console.log("[LOG] Found", jsFiles.length, "Modules");
+  if (logging == "debug") console.log("[DEBUG] Found Modules: ", jsFiles);
+  return jsFiles;
 }
+
+async function loadModule(ModuleName) {
+  if (!modules.includes(ModuleName)) {
+    if (logging == "debug") console.log("[DEBUG] Cant find Module: ", ModuleName);
+    return "Modul nicht gefunden."
+  }
+  const module = await import(path.join(modulesPath, ModuleName + ".js"));
+  if (logging == "debug") console.log("[DEBUG] Loaded new Module: ", ModuleName);
+  return "Modul erfolgreich Importiert"
+}
+
+loadModule("system")
